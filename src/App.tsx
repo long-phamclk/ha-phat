9cd818610f2501a895bbd9c74d61b8e8b87bd8fc
unknown <chiliki.long@gmail.com>
2024-11-04 22:34:40 +1100

    add gioi thieu

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import GioiThieu from "./pages/GioiThieu";
 import SanPham from "./pages/SanPham";
@@ -8,14 +8,16 @@ import LienHe from "./pages/LienHe";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Home />
-      <GioiThieu />
-      <SanPham />
-      <DuAn />
-      <NhanSu />
-      <LienHe />
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/gioithieu" element={<GioiThieu />} />
+        <Route path="/sanpham" element={<SanPham />} />
+        <Route path="/duan" element={<DuAn />} />
+        <Route path="/nhansu" element={<NhanSu />} />
+        <Route path="/lienhe" element={<LienHe />} />
+      </Routes>
+    </Router>
   );
 };
 
--- src/pages/GioiThieu.tsx
+++ src/pages/GioiThieu.tsx
@@ -1,7 +1,161 @@
-import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import styled from "styled-components";
 
 const GioiThieu = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Wrapper>
+        <TitleWrapper>
+          <Title>Giới Thiệu Hà Phát</Title>
+        </TitleWrapper>
+
+        <StyledSection>
+          <Headline>Lịch sử hình thành và phát triển</Headline>
+          <div>
+            <StyledP>
+              Thành lập từ năm 2003 đến nay, CÔNG TY CỔ PHẦN VẬT LIỆU VÀ XÂY
+              DỰNG HÀ PHÁT đã có 17 năm trưởng thành và phát triển. Vượt qua
+              những khó khăn của những ngày đầu, bằng nỗ lực vươn lên, khẳng
+              định bằng uy tín và chất lượng. Hiện nay, CÔNG TY CỔ PHẦN VẬT LIỆU
+              VÀ XÂY DỰNG HÀ PHÁT đã trở thành một thương hiệu được nhiều khách
+              hàng lớn tin dùng.
+            </StyledP>
+            <br></br>
+            <StyledP>
+              Sản phẩm của Công ty đã xuất hiện tại nhiều công trình ở các tỉnh
+              thành trên cả nước, phục vụ nhiều công trình lớn, có yêu cầu kỹ
+              thuật khắt khe. Với tiêu chí uy tín và chất lượng, CÔNG TY CỔ PHẦN
+              VẬT LIỆU VÀ XÂY DƯNG HÀ PHÁT đã chú trọng đầu tư vào nguồn lực.
+              Hiện nay, với đội ngũ công nhân viên ký thuật lành nghề, được đào
+              tạo cơ bản, có chứng chỉ hành nghề đạt chuẩn, Công ty tự hào luôn
+              làm chủ mọi công nghệ và kỹ thuật tiên tiến nhất trong lĩnh vực bê
+              tông thương phẩm.
+            </StyledP>
+            <br></br>
+            <StyledP>
+              Do được kế t hừa từ nhiều công ty đã và đang thành công nhất tại
+              Việt Nam trong lĩnh vực kinh doanh vật liệu và xây dựng nên chúng
+              tôi có nhiều tiềm năng phát triển nhanh hiệu quả. Hiện nay, các
+              hoạt động cung cấp bê tông thương phẩm dịch vụ trọn gói của Công
+              ty Hà Phát luôn đảm bảo thời gian, chất lượng, mang lại sự thành
+              công cho các công trình của khách hàng.
+            </StyledP>
+          </div>
+        </StyledSection>
+        <StyledSection>
+          <Headline>Năng lực công ty</Headline>
+          <div>
+            <StyledP>
+              Hiện tại, công ty có 04 cụm trạm trộn bê tông thương phẩm công
+              suất lớn đang hoạt động sản xuất tại 04 điểm:
+            </StyledP>
+            <ul>
+              <StyledLi>
+                Trạm Hà Phát Hà Nội đặt tại Cụm công nghiệp Lại Yên - Hoài Đức -
+                Hà Nội. Công suất 120 m3/h và 150 m3/h. Công nghệ và thiết bị
+                hiện đại của Đức, vận hành tự động.
+              </StyledLi>
+              <StyledLi>
+                Trạm Hà Phát Hưng Yên đặt tại Khu Đại học Phố Hiến Hưng Yên.
+                Công suất 120 m3/h (02 trạm 60 m3/h). Công nghệ tiên tiến, vận
+                hành tự động.
+              </StyledLi>
+              <StyledLi>
+                Trạm Hà Phát Gia Lâm đặt tại dự án Vinhomes Ocean Park. Công
+                suất 360 m3/h (02 trạm 180 m3/h). Công nghệ, thiết bị của Đức,
+                tự động hóa cao.
+              </StyledLi>
+              <StyledLi>
+                Trạm Hà Phát Tây Mỗ đặt tại dự án Vinhomes Smart City. Công suất
+                300 m3/h (02 trạm 150 m3/h). Công nghệ, thiết bị của Đức.
+              </StyledLi>
+            </ul>
+            <br></br>
+            <StyledP>
+              Với vị trí và hệ thống giao thông thuận lợi. Trạm Hà Phát Hà Nội
+              nằm trên trục đường chính vào trung tâm thành phố và liền kề với
+              khu mở rộng thủ dô nên có thể cung cấp bê tông cho các công trình
+              tại nội thành và các công trình nằm ở phía Tây, phía Nam của Thành
+              Phố. Trạm Hà Phát Hưng Yên có thể cung cấp khu vực Khu Đại học Phố
+              Hiến cùng các vùng lân cận. Trạm Hà Phát Gia Lâm và trạm Hà Phát
+              Tây Mỗ cung cấp cho 2 đại dự án Vinhomes Ocean Park và Vinhomes
+              Smart City.
+            </StyledP>
+          </div>
+        </StyledSection>
+        <StyledSection>
+          <Headline>Chiến lược</Headline>
+          <div>
+            <StyledP>
+              Chiến lược kinh doanh của Hà Phát xem phát triên bền vững là một
+              yếu tố nòng cốt tạo ra giá trị không chỉ bằng việc phát triển kinh
+              tế mà còn gắn liền với những hoạt động về lĩnh vực môi trường và
+              xã hội.
+            </StyledP>
+            <ul>
+              <StyledLi>
+                Đối với Khách hàng: Nỗ lực hết mình vì lợi ích của khách hàng.
+              </StyledLi>
+              <StyledLi>
+                Đối với Người lao động: Đặt lợi ích của người lao động ngang
+                bằng với lợi ích của Công ty.
+              </StyledLi>
+              <StyledLi>
+                Đối với Cộng đồng: Làm cho cuộc sống ngày càng tốt đẹp hơn.
+              </StyledLi>
+              <StyledLi>
+                Đối với Môi trường: Bảo vệ môi trường vì thế hệ tương lai.
+              </StyledLi>
+              <StyledLi>
+                Đối với Cổ đông: Gia tăng giá trị trên cả sự mong đợi của họ.
+              </StyledLi>
+            </ul>
+          </div>
+        </StyledSection>
+      </Wrapper>
+      <Footer />
+    </div>
+  );
 };
 
 export default GioiThieu;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 20px;
+  padding: 0 100px;
+
+  @media (max-width: 768px) {
+    padding: 0 50px;
+  }
+`;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Title = styled.p`
+  font-size: 3rem;
+  color: hsl(144, 84%, 31%);
+  padding-top: 20px;
+`;
+
+const StyledSection = styled.section``;
+
+const Headline = styled.h2`
+  margin: 20px 0px 10px;
+  font-weight: 500;
+`;
+
+const StyledP = styled.p`
+  font-size: 1.1rem;
+  text-indent: 1.5rem;
+`;
+
+const StyledLi = styled.li`
+  font-size: 1.1rem;
+`;
